d42c16cf749676b142e701c52279f2d6d404d814
Mahmoud <mahmoud.timoumi@esprit.tn>
2022-07-08 21:35:37 +0200

    adding Comment feature

---
+++ src/Entity/Comment.php
@@ -0,0 +1,92 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CommentRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CommentRepository::class)
+ */
+class Comment
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $content;
+
+    /**
+     * @ORM\Column(type="datetime_immutable")
+     */
+    private $publishAt;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Artical::class, inversedBy="comments")
+     */
+    private $artical;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=User::class, inversedBy="comments")
+     */
+    private $user;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPublishAt(): ?\DateTimeImmutable
+    {
+        return $this->publishAt;
+    }
+
+    public function setPublishAt(\DateTimeImmutable $publishAt): self
+    {
+        $this->publishAt = $publishAt;
+
+        return $this;
+    }
+
+    public function getArtical(): ?Artical
+    {
+        return $this->artical;
+    }
+
+    public function setArtical(?Artical $artical): self
+    {
+        $this->artical = $artical;
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+}
