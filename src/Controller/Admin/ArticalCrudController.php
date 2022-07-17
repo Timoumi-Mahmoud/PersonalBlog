93d4086d784ee90dd760709d8eb6ced85272e62e
Mahmoud <mahmoud.timoumi@esprit.tn>
2022-07-17 23:03:57 +0200

    *Easy admin configuration & template adjustment

--- src/Controller/Admin/ArticalCrudController.php
+++ src/Controller/Admin/ArticalCrudController.php
@@ -4,6 +4,12 @@ namespace App\Controller\Admin;
 
 use App\Entity\Artical;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
+use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\DateTimeField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ImageField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 
 class ArticalCrudController extends AbstractCrudController
 {
@@ -12,14 +18,23 @@ class ArticalCrudController extends AbstractCrudController
         return Artical::class;
     }
 
-    /*
+
     public function configureFields(string $pageName): iterable
     {
         return [
-            IdField::new('id'),
+           # IdField::new('id'),
             TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+            TextEditorField::new('content'),
+            DateTimeField::new('datePublish'),
+            ImageField::new('image')
+                ->setBasePath('uploads/images')
+                ->setUploadDir('public/uploads/images')
+                ->setUploadedFileNamePattern('[randomhash].[extension]'),
+            AssociationField::new('category')
+
+            ];
     }
-    */
+
+
+
 }
