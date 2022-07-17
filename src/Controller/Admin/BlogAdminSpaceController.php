52e1c8ae7965c0b50097d9d7b3be7b88c0a2af75
Mahmoud <mahmoud.timoumi@esprit.tn>
2022-07-17 13:49:34 +0200

    dashbord and crud for the admin

--- src/Controller/Admin/BlogAdminSpaceController.php
+++ src/Controller/Admin/BlogAdminSpaceController.php
@@ -10,6 +10,8 @@ use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use EasyCorp\Bundle\EasyAdminBundle\Config\UserMenu;
+use Symfony\Component\Security\Core\User\UserInterface;
 
 class BlogAdminSpaceController extends AbstractDashboardController
 {
@@ -30,10 +32,44 @@ class BlogAdminSpaceController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linkToDashboard('Dashboard', 'fa fa-home');
-       yield MenuItem::linkToCrud('The Label', 'fas fa-list', Artical::class);
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', Category::class);
-        yield MenuItem::linkToCrud('The Label', 'fas fa-list', User::class);
+       yield MenuItem::linkToCrud('Blog Posts', 'fa fa-file-text', Artical::class);
+        yield MenuItem::linkToCrud('Categories', 'fa fa-tags', Category::class);
+        yield MenuItem::linkToCrud('Users', 'fa fa-user', User::class);
+        MenuItem::linkToLogout('Logout', 'fa fa-exit');
+        MenuItem::linkToExitImpersonation('Stop impersonation', 'fa fa-exit');
 
 
     }
+    public function configureUserMenu(UserInterface $user): UserMenu
+    {
+        // Usually it's better to call the parent method because that gives you a
+        // user menu with some menu items already created ("sign out", "exit impersonation", etc.)
+        // if you prefer to create the user menu from scratch, use: return UserMenu::new()->...
+        return parent::configureUserMenu($user)
+            // use the given $user object to get the user name
+            ->setName($user->getUsername())
+            // use this method if you don't want to display the name of the user
+            ->displayUserName(false)
+
+            // you can return an URL with the avatar image
+            ->setAvatarUrl('https://...')
+            //->setAvatarUrl($user->getProfileImageUrl())
+            // use this method if you don't want to display the user image
+            ->displayUserAvatar(false)
+            // you can also pass an email address to use gravatar's service
+           ->setGravatarEmail($user->getEmail())
+
+            // you can use any type of menu item, except submenus
+            ->addMenuItems([
+                MenuItem::linkToRoute('My Profile', 'fa fa-id-card', '...', ['...' => '...']),
+                MenuItem::linkToRoute('Settings', 'fa fa-user-cog', '...', ['...' => '...']),
+                MenuItem::section(),
+                MenuItem::linkToLogout('Logout', 'fa fa-sign-out'),
+            ]);
+    }
+
+
+
+
+
 }
